b3f5c6bcdf07279ec7180e03b9fff99b047fca35
sachin kumar <goputen215@gmail.com>
2025-08-07 23:22:11 +0530

    update

---
+++ dynamicProgramming/02_findLongestChain.py
@@ -0,0 +1,20 @@
+from collections import List
+class Solution:
+    def dp(self , idx , prev , n , pairs , memo):
+        if idx >=n:
+            return 0
+        take = skip = 0
+        if (idx , prev) in memo:
+            return memo[(idx , prev)]
+
+        skip = self.dp(idx + 1 ,prev , n , pairs, memo)
+        if prev == -1 or pairs[prev][1] < pairs[idx][0]:
+            take = self.dp( idx +1 , idx , n , pairs , memo) + 1
+
+        memo[(idx,prev)] = max(skip , take)
+        return max(skip , take)
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        n = len(pairs)
+        pairs.sort(key = lambda x:x[1])
+        memo = {}
+        return self.dp( 0 , -1 , n , pairs , memo )
---
+++ dynamicProgramming/03_longestStrChain.py
@@ -0,0 +1,27 @@
+from typing import List
+class Solution:
+    def check ( self , bigStr , smallStr):
+        if len(bigStr) - len(smallStr) != 1:return False
+        for i in range(len(bigStr)):
+            newStr = bigStr[:i] + bigStr[i+1:]
+            if newStr == smallStr:return True
+        return False
+
+    def dp( self , idx , prev , n , words , memo):
+        if idx >= n:
+            return 0
+        take = skip =0
+        if memo[idx][prev+1] != -1:
+            return memo[idx][prev+1]
+
+        skip = self.dp( idx +1 , prev , n , words , memo)
+        if prev == -1 or self.check( words[idx] , words[prev] ):
+            take = self.dp( idx + 1 , idx , n ,words , memo) + 1
+        memo[idx][prev+1] = max(take , skip)
+        return max(take ,skip)
+        
+    def longestStrChain(self, words: List[str]) -> int:
+        n = len(words)
+        words.sort(key = len)
+        memo = [ [ -1] * (n+1) for _ in range(n+1)]
+        return self.dp( 0 , -1 , n , words , memo)
