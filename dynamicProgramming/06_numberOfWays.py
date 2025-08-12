31a7d5ab6e28b46970484a3cfa7bed80d44b9961
sachin <goputen215@gmail.com>
2025-08-12 17:27:25 +0530

    Create 06_numberOfWays.py

---
+++ dynamicProgramming/06_numberOfWays.py
@@ -0,0 +1,20 @@
+"2787. Ways to Express an Integer as Sum of Powers"
+class Solution:
+    def dp(self, num, n, x, memo):
+        if n == 0:
+            return 1
+        if n < 0:
+            return 0
+        val = num  x
+        if val > n:
+            return 0
+        if memo[num][n] != -1:
+            return memo[num][n]
+        take = self.dp(num + 1, n - val, x, memo)
+        skip = self.dp(num + 1 , n, x, memo)
+        memo[num][n] = (take + skip) % (10  9 + 7)
+        return memo[num][n]
+        
+    def numberOfWays(self, n: int, x: int) -> int:
+        memo = [[-1] * (301) for _ in range(301)]
+        return self.dp(1, n, x, memo)
